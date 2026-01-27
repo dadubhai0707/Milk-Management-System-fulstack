e749f1f4ce874a255f42390a9335699cb8c646d7
dadubhai0707 <sanjaysuthar121212@gmail.com>
2026-01-27 12:01:57 +0530

    bottom navigation only admin side update

--- Frontend/src/navigation/AdminNavigator.js
+++ Frontend/src/navigation/AdminNavigator.js
@@ -1,11 +1,17 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AdminDashboard from "../screens/Store/AdminDashboard";
+import AdminBottomTabs from "./BottomTabs";
 
 const Stack = createNativeStackNavigator();
 
 export default function AdminStack() {
   return (
     <Stack.Navigator >
+      {/* Bottom Navigation */}
+      <Stack.Screen
+        name="AdminHome"
+        component={AdminBottomTabs}
+      />
       <Stack.Screen name="AdminDashboard" component={AdminDashboard} />
     </Stack.Navigator>
   );
--- Frontend/src/navigation/BottomTabs.js
+++ Frontend/src/navigation/BottomTabs.js
@@ -1,21 +1,11 @@
-import AdminStack from "./AdminNavigator";
-import SellerStack from "./SellerNavigator";
-import CustomerStack from "./CustomerNavigator";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import AdminDashboard from "../screens/Store/AdminDashboard";
 const Tab = createBottomTabNavigator();
 
-export default function BottomTabs({ role }) {
-
-  const getHomeScreen = () => {
-    if (role === "owner") return AdminStack;
-    if (role === "seller") return SellerStack;
-    return CustomerStack;
-  };
-
+export default function BottomNavigation() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={getHomeScreen()} />
+      <Tab.Screen name="Dashboard" component={AdminDashboard} />
     </Tab.Navigator>
   );
 }
